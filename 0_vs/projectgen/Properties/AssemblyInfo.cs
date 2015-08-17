ab6f89066b6cebb7899c522a094a5f70fd208e8d
Vincent Mahnke <accounts@vimaster.de>
2015-08-17 23:16:19 +0200

    + Added name to AssemblyInfo-copyright

--- 0_vs/projectgen/Properties/AssemblyInfo.cs
+++ 0_vs/projectgen/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("projectgen")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Vincent Mahnke 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
